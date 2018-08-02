1298718360489cc146040e572c07826f095e205b
KyleADay <kday@scottlogic.co.uk>
2018-08-02 13:59:58 +0100

    talk to the hand

---
+++ ui/helpers/page_objects/ConfirmItemPage.js
@@ -0,0 +1,7 @@
+const Page = require('./Page');
+
+module.exports.url = `${Page.baseURL}/confirmItem`;
+
+module.exports.handImage = {
+    class: 'hand' //This isn't a great selector
+}
--- ui/helpers/users/HonestyStoreUser.js
+++ ui/helpers/users/HonestyStoreUser.js
@@ -2,6 +2,7 @@ const {User} = require('./User');
 const HomePage = require('../page_objects/HomePage'); //TODO move all page requires to separate helper
 const DisclaimerPage = require('../page_objects/DisclaimerPage');
 const ScanItemPage = require('../page_objects/ScanItemPage');
+const ConfirmItemPage = require('../page_objects/ConfirmItemPage');
 const path = require('path');
 const sleep = (ms) => new Promise((resolve) => {
 setTimeout(resolve, ms);
@@ -16,6 +17,11 @@ class HonestyStoreUser extends User {
     async injectWebcam() {
         await sleep(10000); //TODO tidy me
         return await this.webDriver.executeScript('injectWebcam({isDetecting: false, cameraConnected: true})');
+    }
+
+    async removeHand(){
+        await sleep(20000); //TODO tidy me
+        return await this.webDriver.executeScript("document.getElementsByClassName('hand')[0].setAttribute('style', 'display:none');");
     }
 
     //TODO move page specific actions into separate page 'actions' class
@@ -44,6 +50,7 @@ class HonestyStoreUser extends User {
     async uploadsFile(filePath = `${path.resolve(__dirname)}\\..\\..\\assets\\thinking_man.png`) {
         let element = await this.getElementById(ScanItemPage.fileUpload.browse.id);
         element.sendKeys(filePath);
+        // await sleep(10000); //TODO tidy me
     }
 }
 
